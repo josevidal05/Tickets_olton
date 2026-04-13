603b55d43b90a8025656de39f1b819532bd8dd45
josevidal05 <josevidalfernandez5@gmail.com>
2026-04-13 09:49:42 +0200

    Creada clase ticket

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Ticket(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    nombre_empresa = models.CharField(max_length=150)
+
+    TIPO_DISPOSITIVO_CHOICES = [
+        ('maquina', 'Máquina'),
+        ('tracker', 'Tracker'),
+        ('otro', 'Otro'),
+    ]
+
+    tipo_dispositivo = models.CharField(
+        max_length=20,
+        choices=TIPO_DISPOSITIVO_CHOICES
+    )
+    id_dispositivo = models.CharField(max_length=100)
+    duda = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
