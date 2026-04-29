4ff1fc062bb8a3b38b0f08745882716d3f7d9060
josevidal05 <josevidalfernandez5@gmail.com>
2026-04-29 14:36:41 +0200

    nuevas clases Empresa y Usuario

--- proyecto/app/admin.py
+++ proyecto/app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Ticket
+from .models import Ticket, User, Empresa
 # Register your models here.
 
 admin.site.register(Ticket)
+admin.site.register(User)
+admin.site.register(Empresa)
